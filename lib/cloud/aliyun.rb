e73c0d17ee9549200c0bcecfc48bae03b9bb202d
yuchao <jianxi.yc@alibaba-inc.com>
2016-03-16 14:26:12 +0800

    create

--- lib/cloud/aliyun.rb
+++ lib/cloud/aliyun.rb
@@ -9,7 +9,13 @@ require "common/common"
 
 require "cloud"
 require "cloud/aliyun/cloud"
-require "cloud/aliyun/stemcell_manager"
+require "cloud/aliyun/aliyunConstants"
+require "cloud/aliyun/aliyunHttpsUtil"
+require "cloud/aliyun/aliyunDiskWrapper"
+require "cloud/aliyun/aliyunImgWrapper"
+require "cloud/aliyun/aliyunInstanceWrapper"
+require "cloud/aliyun/aliyunSecurityGroupWrapper"
+require "cloud/aliyun/aliyunSnapshotWrapper"
 
 module Bosh
   module Clouds
