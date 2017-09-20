9f98cd725524a976ac0b8bfed19435085f85d9f8
ailan.gl <ailan.gl@alibaba-inc.com>
2017-09-20 20:37:22 +0800

    first golang edition
    Former-commit-id: 7fb47f13d6790f64ffae376d056726e46a74d44a [formerly c254c237c0eb152cbe0b081d14e6896cfd96e5e4]
    Former-commit-id: 47c5847be8135b8d3e0e1857ed82c12297783bb0

---
+++ src/bosh-alicloud-cpi/action/has_disk.go
@@ -0,0 +1,39 @@
+package action
+
+import (
+	bosherr "github.com/cloudfoundry/bosh-utils/errors"
+	"github.com/cppforlife/bosh-cpi-go/apiv1"
+
+	"bosh-alicloud-cpi/alicloud"
+	"github.com/denverdino/aliyungo/ecs"
+	"github.com/denverdino/aliyungo/common"
+)
+
+type HasDiskMethod struct {
+	runner alicloud.Runner
+}
+
+func NewHasDiskMethod(runner alicloud.Runner) HasDiskMethod {
+	return HasDiskMethod{runner}
+}
+
+func (a HasDiskMethod) HasDisk(cid apiv1.DiskCID) (bool, error) {
+	client := a.runner.NewClient()
+	instid := cid.AsString()
+
+	var args ecs.DescribeDisksArgs
+	args.RegionId = common.Region(a.runner.Config.OpenApi.RegionId)
+	args.DiskIds = []string {cid.AsString()}
+
+	disks, _, err := client.DescribeDisks(&args)
+
+	if err != nil {
+		return false, bosherr.WrapErrorf(err, "DescribeDisks failed cid=%s", instid)
+	}
+
+	if len(disks) == 0 {
+		return false, nil
+	} else {
+		return true, nil
+	}
+}
---
+++ src/bosh-alicloud-cpi/alicloud/runner.go
@@ -0,0 +1,127 @@
+package alicloud
+
+import (
+	bosherr "github.com/cloudfoundry/bosh-utils/errors"
+	"github.com/cloudfoundry/bosh-utils/logger"
+	"github.com/denverdino/aliyungo/ecs"
+	"github.com/denverdino/aliyungo/common"
+	"strings"
+	"fmt"
+	"time"
+)
+
+const (
+	USE_FORCE_STOP = true
+	DEFAULT_TIMEOUT = 1200000
+	DEFAULT_WAIT_INTERVAL = 500
+)
+
+type Runner struct {
+	Logger logger.Logger
+	Config Config
+}
+
+func NewRunner(logger logger.Logger, config Config) (Runner) {
+	return Runner{
+		Logger: logger,
+		Config: config,
+	}
+}
+
+func (a Runner) NewClient() (* ecs.Client) {
+	return ecs.NewClient(a.Config.OpenApi.AccessKeyId, a.Config.OpenApi.AccessKeySecret)
+}
+
+func (a Runner) FindStemcellId() (string, error) {
+	c := a.Config
+	for _, region := range c.OpenApi.Regions {
+		if strings.Compare(region.Name, c.OpenApi.RegionId) == 0 {
+			return region.ImageId, nil
+		}
+	}
+	return "", fmt.Errorf("Unknown Region")
+}
+
+func (a Runner) GetInstance(instid string) (* ecs.InstanceAttributesType, error) {
+	client := a.NewClient()
+
+	var args ecs.DescribeInstancesArgs
+	args.RegionId = common.Region(a.Config.OpenApi.RegionId)
+	args.InstanceIds = "[\"" + instid + "\"]"
+
+	insts, _, err := client.DescribeInstances(&args)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(insts) == 0 {
+		return nil, nil
+	}
+
+	return &insts[0], nil
+}
+
+func (a Runner) GetInstanceStatus(instid string) (ecs.InstanceStatus, error) {
+	inst, err := a.GetInstance(instid)
+
+	if err != nil {
+		return "", err
+	}
+
+	if inst == nil {
+		return "", bosherr.Error("Missing Instance: id=" + instid)
+	}
+
+	return inst.Status, nil
+}
+
+func (a Runner) WaitForInstanceStatus(instid string, to_status ecs.InstanceStatus) (error) {
+	timeout := DEFAULT_TIMEOUT
+	for {
+		status, err := a.GetInstanceStatus(instid)
+
+		if err != nil {
+			return err
+		}
+
+		if status == to_status {
+			return nil
+		}
+
+		if timeout > 0 {
+			timeout -= 1000
+			time.Sleep(time.Duration(DEFAULT_WAIT_INTERVAL) * time.Millisecond)
+		} else {
+			return bosherr.Error("WaitForInstanceStatus timeout")
+		}
+	}
+}
+
+func (a Runner) StopInstance(instid string) (error) {
+	client := a.NewClient()
+	return client.StopInstance(instid, USE_FORCE_STOP)
+}
+
+func (a Runner) StartInstance(instid string) (error) {
+	client := a.NewClient()
+	return client.StartInstance(instid)
+}
+
+func (a Runner) RebootInstance(instid string) (error) {
+	client := a.NewClient()
+	return client.RebootInstance(instid, USE_FORCE_STOP)
+}
+
+//func (a Runner) HasDisk(diskid string) (error) {
+//	client := a.NewClient()
+//	return client.DescribeDisks()
+//}
+
+//func (a Runner) CreateDisk() {
+//	client := a.NewClient()
+//	var args ecs.DescribeVSwitchesArgs;
+//	args.RegionId
+//	client.DescribeVSwitches()
+//
+//}
