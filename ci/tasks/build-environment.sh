421841a500be7106a6c04f817c325775c03a2ef4
He Guimin <guimin.hgm@alibaba-inc.com>
2017-10-13 11:25:25 +0800

    debug build environment

--- ci/tasks/build-environment.sh
+++ ci/tasks/build-environment.sh
@@ -16,8 +16,8 @@ METADATA=metadata
 TERRAFORM_VERSION=0.10.0
 TERRAFORM_PROVIDER_VERSION=1.2.4
 
-curl -O https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-curl -O https://github.com/alibaba/terraform-provider/releases/download/V${TERRAFORM_PROVIDER_VERSION}/terraform-provider-alicloud_linux-amd64.tgz
+wget -N https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+wget -N https://github.com/alibaba/terraform-provider/releases/download/V${TERRAFORM_PROVIDER_VERSION}/terraform-provider-alicloud_linux-amd64.tgz
 mkdir -p ${TERRAFORM_PATH}
 ls -l ./
 ls -l ../
