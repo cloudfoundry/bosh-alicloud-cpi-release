37fcff1e3eeacbb9bccfa8833c01a43228ec5860
He Guimin <guimin.hgm@alibaba-inc.com>
2017-10-18 18:28:59 +0800

    git remote

--- ci/tasks/build-environment.sh
+++ ci/tasks/build-environment.sh
@@ -69,22 +69,23 @@ function copyToOutput(){
 #            echo $'\n'
 #            echo "${GIT_USER_PASSWORD}"
 #            echo $'\n'
-            echo "#!/bin/bash " > git_pull.sh
-            echo $'\n' >> git_pull.sh
-            echo git pull https concourse_ci_tmp  \<\< EOF >> git_pull.sh
-            echo sleep 2 >> git_pull.sh
-#            echo read -p "Username for 'https://github.com':" user >> git_pull.sh
-#            echo read -p "Password for 'https://${GIT_USER_ID}@github.com':" pass >> git_pull.sh
-#            echo you have entered $no, $name
-            echo "${GIT_USER_ID}" >> git_pull.sh
-            echo sleep 2 >> git_pull.sh
-            echo "${GIT_USER_PASSWORD}" >> git_pull.sh
+#            echo "#!/bin/bash " > git_pull.sh
 #            echo $'\n' >> git_pull.sh
-            echo EOF >> git_pull.sh
-            cat git_pull.sh
-            chmod +x git_pull.sh
-            ./git_pull.sh
+#            echo git pull https concourse_ci_tmp  \<\< EOF >> git_pull.sh
+#            echo sleep 2 >> git_pull.sh
+##            echo read -p "Username for 'https://github.com':" user >> git_pull.sh
+##            echo read -p "Password for 'https://${GIT_USER_ID}@github.com':" pass >> git_pull.sh
+##            echo you have entered $no, $name
+#            echo "${GIT_USER_ID}" >> git_pull.sh
+#            echo sleep 2 >> git_pull.sh
+#            echo "${GIT_USER_PASSWORD}" >> git_pull.sh
+##            echo $'\n' >> git_pull.sh
+#            echo EOF >> git_pull.sh
+#            cat git_pull.sh
+#            chmod +x git_pull.sh
+#            ./git_pull.sh
 
+            echo -e "${GIT_USER_PASSWORD}" | git pull https://xiaozhu36@github.com/xiaozhu36/bosh-alicloud-cpi-release.git concourse_ci_tmp
             echo "******** git add and commit ********"
             git add .
             git commit -m 'create environment commit'
