4816585e2d759ed774172ca3ae12f219f47c96f6
demon.wy <demon.wy@alibaba-inc.com>
2017-10-20 07:09:02 +0800

    support terraform provision

--- ci/tasks/build-environment.sh
+++ ci/tasks/build-environment.sh
@@ -8,6 +8,10 @@ set -e
 : ${DESTROY_ENVIRONMENT:?}
 : ${GIT_USER_EMAIL:?}
 : ${GIT_USER_NAME:?}
+: ${GIT_USER_ID:?}
+: ${GIT_USER_PASSWORD:?}
+: ${BOSH_REPO_HOST:?}
+: ${BOSH_REPO_BRANCH:?}
 
 CURRENT_PATH=$(pwd)
 SOURCE_PATH=$CURRENT_PATH/bosh-cpi-src
@@ -17,6 +21,7 @@ TERRAFORM_METADATA=$CURRENT_PATH/terraform-metadata
 METADATA=metadata
 TERRAFORM_VERSION=0.10.0
 TERRAFORM_PROVIDER_VERSION=1.2.4
+
 
 wget -N https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 wget -N https://github.com/alibaba/terraform-provider/releases/download/V${TERRAFORM_PROVIDER_VERSION}/terraform-provider-alicloud_linux-amd64.tgz
@@ -31,9 +36,27 @@ export PATH="${TERRAFORM_PATH}:$PATH"
 
 cd ${TERRAFORM_MODULE}
 
-rm -rf ${METADATA}
+echo "******** git install expect ********"
+sudo apt-get install expect -y
+
+echo "******** git pull by https ********"
+echo "#!/usr/bin/expect" > git_install.sh
+echo "spawn git fetch https://${GIT_USER_ID}@${BOSH_REPO_HOST} ${BOSH_REPO_BRANCH}" >> git_install.sh
+echo "expect \"Password for 'https://${GIT_USER_ID}@github.com': \"" >> git_install.sh
+echo "send \"${GIT_USER_PASSWORD}\r\"" >> git_install.sh
+echo "expect eof" >> git_install.sh
+echo exit >> git_install.sh
+chmod +x git_install.sh
+./git_install.sh
+rm -rf ./git_install.sh
+
+echo $'\n'
+echo "****** git merge ******"
+git merge FETCH_HEAD
+
 touch ${METADATA}
 
+echo $'\n'
 echo "Build terraform environment......"
 
 terraform init && terraform apply -var alicloud_access_key=${ALICLOUD_ACCESS_KEY_ID} -var alicloud_secret_key=${ALICLOUD_SECRET_ACCESS_KEY} -var alicloud_region=${ALICLOUD_DEFAULT_REGION}
@@ -51,9 +74,46 @@ function copyToOutput(){
     git config --global user.name ${GIT_USER_NAME}
     git config --local -l
 
+    git status | sed -n 'p' |while read LINE
+    do
+        echo "echo LINE: $LINE"
+        if [[ $LINE == HEAD*detached* ]];
+        then
+            echo "****** fix detached branch ******"
+            read -r -a Words <<< $LINE
+
+            git status | sed -n 'p' |while read LI
+            do
+                echo "echo LI: $LI"
+                if [[ $LI == Changes*not*staged*for*commit* ]];
+                then
+                    git add .
+                    git commit -m 'build environment commit on detached'
+                    git branch temp
+                    git checkout ${BOSH_REPO_BRANCH}
+                    git merge temp
+                    git branch
+                    git branch -d temp
+                fi
+            done
+            break
+        fi
+    done
+
+    echo "******* git status ******"
     git status
-    git add .
-    git commit -m 'commit metadata'
+
+    git status | sed -n '$p' |while read LINE
+    do
+        echo $LINE
+        if [[ $LINE != nothing*clean ]];
+        then
+            echo $LINE
+            git add .
+            git commit -m 'build environment commit'
+            return 0
+        fi
+    done
 
     git status
     return 0
@@ -67,6 +127,7 @@ fi
 
 terraform state list > all_state
 echo "Write metadata ......"
+echo "" > $METADATA
 cat all_state | while read LINE
 do
     if [ $LINE == "alicloud_vswitch.default" ];
--- ci/tasks/destroy-environment.sh
+++ ci/tasks/destroy-environment.sh
@@ -7,6 +7,9 @@ set -e
 : ${ALICLOUD_DEFAULT_REGION:?}
 : ${GIT_USER_EMAIL:?}
 : ${GIT_USER_NAME:?}
+: ${GIT_USER_ID:?}
+: ${GIT_USER_PASSWORD:?}
+: ${BOSH_REPO_HOST:?}
 
 CURRENT_PATH=$(pwd)
 SOURCE_PATH=$CURRENT_PATH/bosh-cpi-src
@@ -16,6 +19,7 @@ TERRAFORM_METADATA=$CURRENT_PATH/terraform-metadata
 METADATA=metadata
 TERRAFORM_VERSION=0.10.0
 TERRAFORM_PROVIDER_VERSION=1.2.4
+
 
 wget -N https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
 wget -N https://github.com/alibaba/terraform-provider/releases/download/V${TERRAFORM_PROVIDER_VERSION}/terraform-provider-alicloud_linux-amd64.tgz
@@ -30,6 +34,25 @@ export PATH="${TERRAFORM_PATH}:$PATH"
 
 cd ${TERRAFORM_MODULE}
 
+echo "******** git install expect ********"
+sudo apt-get install expect -y
+
+echo "******** git pull by https ********"
+echo "#!/usr/bin/expect" > git_install.sh
+echo "spawn git fetch https://${GIT_USER_ID}@${BOSH_REPO_HOST} ${BOSH_REPO_BRANCH}" >> git_install.sh
+echo "expect \"Password for 'https://${GIT_USER_ID}@github.com': \"" >> git_install.sh
+echo "send \"${GIT_USER_PASSWORD}\r\"" >> git_install.sh
+echo "expect eof" >> git_install.sh
+echo exit >> git_install.sh
+chmod +x git_install.sh
+./git_install.sh
+rm -rf ./git_install.sh
+
+echo $'\n'
+echo "****** git merge ******"
+git merge FETCH_HEAD
+
+echo $'\n'
 echo "Destroy terraform environment......"
 terraform init
 echo terraform destroy -var alicloud_access_key=${ALICLOUD_ACCESS_KEY_ID} -var alicloud_secret_key=${ALICLOUD_SECRET_ACCESS_KEY} -var alicloud_region=${ALICLOUD_DEFAULT_REGION}  \<\< EOF > terraform_destroy.sh
@@ -39,6 +62,7 @@ chmod +x terraform_destroy.sh
 ./terraform_destroy.sh
 echo "Destroy terraform environment successfully."
 rm -rf ./terraform_destroy.sh
+
 
 function copyToOutput(){
 
@@ -51,9 +75,47 @@ function copyToOutput(){
     git config --global user.name ${GIT_USER_NAME}
     git config --local -l
 
+    git status | sed -n 'p' |while read LINE
+    do
+        echo "echo LINE: $LINE"
+        if [[ $LINE == HEAD*detached* ]];
+        then
+            echo "****** fix detached branch ******"
+            read -r -a Words <<< $LINE
+
+            git status | sed -n 'p' |while read LI
+            do
+                echo "echo LI: $LI"
+                if [[ $LI == Changes*not*staged*for*commit* ]];
+                then
+                    git add .
+                    git commit -m 'destroy environment commit on detached'
+                    git branch temp
+                    git checkout ${BOSH_REPO_BRANCH}
+                    git merge temp
+                    git branch
+                    git branch -d temp
+                fi
+            done
+            break
+        fi
+    done
+
+    echo "******* git status ******"
     git status
-    git add .
-    git commit -m 'commit metadata'
+
+    git status | sed -n '$p' |while read LINE
+    do
+        echo $LINE
+        if [[ $LINE != nothing*clean ]];
+        then
+            echo $LINE
+            git add .
+            git commit -m 'destroy environment commit -a'
+            return 0
+        fi
+    done
+
     git status
     return 0
 }
