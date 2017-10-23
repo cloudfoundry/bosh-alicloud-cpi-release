9306d710df0e4166841e13738882f6adf10e7e78
demon.wy <demon.wy@alibaba-inc.com>
2017-10-23 22:04:56 +0800

    build cpi archieve

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -30,7 +30,7 @@ pushd candidate/repo
   echo "******** git pull by https ********"
   echo "#!/usr/bin/expect" > git_install.sh
   echo "spawn git fetch https://${GIT_USER_ID}@${BOSH_REPO_HOST} ${BOSH_REPO_BRANCH}" >> git_install.sh
-  echo "expect \"Password for 'https://${GIT_USER_ID}@github.com': \"" >> git_install.sh
+  echo "expect \"Password for 'https://${GIT_USER_ID}@gitlab.com': \"" >> git_install.sh
   echo "send \"${GIT_USER_PASSWORD}\r\"" >> git_install.sh
   echo "expect eof" >> git_install.sh
   echo exit >> git_install.sh
