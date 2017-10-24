5d8cbf9f5f4268554413a11d53f59991f88ac79d
demon.wy <demon.wy@alibaba-inc.com>
2017-10-24 12:10:33 +0800

    build cpi archieve

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -24,22 +24,7 @@ mv ./bosh-cli-* /usr/local/bin/bosh2
 cp -r bosh-cpi-src candidate/repo
 
 pushd candidate/repo
-  echo "******** git install expect ********"
-  sudo apt-get install expect -y
-
-  echo "******** git pull by https ********"
-  echo "#!/usr/bin/expect" > git_install.sh
-  echo "spawn git fetch https://${GIT_USER_ID}@${BOSH_REPO_HOST} ${BOSH_REPO_BRANCH}" >> git_install.sh
-  echo "expect \"Password for 'https://${GIT_USER_ID}@gitlab.com': \"" >> git_install.sh
-  echo "send \"${GIT_USER_PASSWORD}\r\"" >> git_install.sh
-  echo "expect eof" >> git_install.sh
-  echo exit >> git_install.sh
-  chmod +x git_install.sh
-  ./git_install.sh > log
-  cat log
-  rm -rf ./git_install.sh
-
-  echo $'\n'
+  git fetch https://${GIT_USER_ID}:${GIT_USER_PASSWORD}@${BOSH_REPO_HOST} ${BOSH_REPO_BRANCH}
   echo "****** git merge ******"
   git merge FETCH_HEAD
 
