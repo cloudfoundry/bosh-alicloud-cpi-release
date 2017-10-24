ba7688ef234f842f430d710e42942fa7a451d35e
demon.wy <demon.wy@alibaba-inc.com>
2017-10-24 13:03:25 +0800

    build cpi archieve

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -15,26 +15,20 @@ source /etc/profile.d/chruby-with-ruby-2.1.2.sh
 cpi_release_name="bosh-alicloud-cpi"
 semver=`cat version-semver/number`
 
+CURRENT_PATH=$(pwd)
+DESC=$CURRENT_PATH/bosh-cpi-dev-artifacts/dev-release-artifacts/
+
 # install bosh
 echo "Installing Bosh CLI..."
 curl -O https://s3.amazonaws.com/bosh-cli-artifacts/bosh-cli-2.0.28-linux-amd64
 chmod +x ./bosh-cli-*
 mv ./bosh-cli-* /usr/local/bin/bosh2
 
-cp -r bosh-cpi-src candidate/repo
-
 # todo: get email and user from params
 git config --global user.email "demon.wy@alibaba-inc.com"
 git config --global user.name "demonwy"
 
-pushd candidate/repo
-  git fetch https://${GIT_USER_ID}:${GIT_USER_PASSWORD}@${BOSH_REPO_HOST} ${BOSH_REPO_BRANCH}
-  git status
-  git add .
-  git commit -m 'ci build candidate'
-  echo "****** git merge ******"
-  git merge FETCH_HEAD
-
+pushd bosh-cpi-src
   echo "using bosh CLI version..."
   bosh2 -v
 
@@ -63,11 +57,11 @@ pushd candidate/repo
   # refers: https://bosh.io/docs/cli-v2#create-release
   bosh2 create-release --name $cpi_release_name --version $semver --tarball $cpi_release_name-$semver.tgz
 
-  ls -al dev-release-artifacts
+  ls -al ${DESC}
   mkdir dev-release-artifacts
-  rm -rf dev-release-artifacts/*
-  mv $cpi_release_name-$semver.tgz dev-release-artifacts/
-  ls -al dev-release-artifacts
+  rm -rf ${DESC}/*
+  mv $cpi_release_name-$semver.tgz ${DESC}/
+  ls -al ${DESC}/
 
   echo "git status..."
   git status
@@ -75,3 +69,5 @@ pushd candidate/repo
   git add .
   git commit -m 'create cpi release'
 popd
+
+cp -r bosh-cpi-dev-artifacts candidate/repo
