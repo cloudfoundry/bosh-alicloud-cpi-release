f85f69f7a4f07431be6b899b6b968f2fa65d9644
He Guimin <guimin.hgm@alibaba-inc.com>
2017-10-24 11:36:36 +0800

    debug candidate

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -40,10 +40,10 @@ pushd candidate/repo
   echo "#!/usr/bin/expect" > git_install.sh
   echo "spawn git fetch https://${GIT_USER_ID}@${BOSH_REPO_HOST} ${BOSH_REPO_BRANCH}" >> git_install.sh
   echo "expect \"Password for 'https://${GIT_USER_ID}@gitlab.com': \"" >> git_install.sh
-  echo "send \"${GIT_USER_PASSWORD}\r\"" >> git_install.sh
+  echo "send \"${GIT_USER_PASSWORD}\n\"" >> git_install.sh
+  echo "expect \"*#\"" >> git_install.sh
   echo "expect eof" >> git_install.sh
-  echo "expect EOF" >> git_install.sh
-  echo exit >> git_install.sh
+  echo EOF >> git_install.sh
   cat git_install.sh
   chmod +x git_install.sh
   ./git_install.sh > log2
