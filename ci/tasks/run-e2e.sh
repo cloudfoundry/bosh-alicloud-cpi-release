a54e245ab43b70413b0b85fee9c191e6de618826
demon.wy <demon.wy@alibaba-inc.com>
2017-11-13 18:20:10 +0800

    add blob for e2e manually

--- ci/tasks/run-e2e.sh
+++ ci/tasks/run-e2e.sh
@@ -2,14 +2,36 @@
 
 set -e
 
+CURRENT_PATH=$(pwd)
+SDK_SLB_BLOB_PATH=$CURRENT_PATH/slb-blob
+SDK_CORE_BLOB_PATH=$CURRENT_PATH/core-blob
+PIP_BLOB_PATH=$CURRENT_PATH/pip-blob
+PYTHON_DEVEL_BLOB_PATH=$CURRENT_PATH/python-blob
+LIBPQXX_DEVEL_BLOB_PATH=$CURRENT_PATH/libpqxx-blob
+LIBFFI_DEVEL_BLOB_PATH=$CURRENT_PATH/libffi-blob
+JQ_BLOB_PATH=$CURRENT_PATH/jq-blob
+
 bosh_cli=$(realpath bosh-cli/bosh-cli-*)
 chmod +x $bosh_cli
 cp "${bosh_cli}" /usr/local/bin/bosh2
 
 source director-state/director.env
 
+# add blobs for e2e test
+# when e2e-test-release/config/final set to remote storage, this code can be removed
+function addBlobs {
+   bosh add-blob ${JQ_BLOB_PATH}/jq-linux64 jq/jq-linux64
+   bosh add-blob ${LIBFFI_DEVEL_BLOB_PATH}/libffi-devel-3.0.13-18.el7.x86_64.rpm libffi_devel/libffi-devel-3.0.13-18.el7.x86_64.rpm
+   bosh add-blob ${LIBPQXX_DEVEL_BLOB_PATH}/libpqxx-devel-4.0.1-1.el7.x86_64.rpm libpqxx_devel/libpqxx-devel-4.0.1-1.el7.x86_64.rpm
+   bosh add-blob ${PYTHON_DEVEL_BLOB_PATH}/aliyun-python-sdk-slb-3.2.1.tar.gz sdk-slb/aliyun-python-sdk-slb-3.2.1.tar.gz
+   bosh add-blob ${PIP_BLOB_PATH}/pip-1.3.1.tar.gz pip/pip-1.3.1.tar.gz
+   bosh add-blob ${SDK_CORE_BLOB_PATH}/aliyun-python-sdk-core-2.5.2.tar.gz sdk_core/aliyun-python-sdk-core-2.5.2.tar.gz
+   bosh add-blob ${SDK_SLB_BLOB_PATH}/aliyun-python-sdk-slb-3.2.1.tar.gz sdk_slb/aliyun-python-sdk-slb-3.2.1.tar.gz
+}
+
 # CREATE TEST RELEASE
 pushd bosh-cpi-src/ci/assets/e2e-test-release
+  addBlobs
   time bosh2 -n create-release --force --name e2e-test --version 1.0.0
   time bosh2 -n upload-release
 popd
